4dce774accdd8d9df90fd310e1e4b989b396cdbb
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-04-26 16:44:28 +0100

    indeed

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,9 +1,16 @@
 import Head from 'next/head'
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
     <>
-    
+      <Head>
+        <title>Peace | Software Developer</title>
+        <meta name="description" content="I'm a Full Stack Developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
     </>
   )
 }
