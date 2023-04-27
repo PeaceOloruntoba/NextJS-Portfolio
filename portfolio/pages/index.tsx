3b73e3a3b50860609b32b8c5e930dfae3e1bb0e3
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-04-27 11:43:19 +0100

    progress

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <Main />
     </>
   )
 }
