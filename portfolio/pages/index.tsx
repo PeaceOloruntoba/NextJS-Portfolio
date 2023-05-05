c89c5b4ac185b372fdac3a21bf498e1c594b5336
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-05 13:53:55 +0100

    progress commit

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import About from "./components/About"
 import Skills from "./components/Skills"
+import Projects from "./components/Projects"
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <Main />
       <About />
       <Skills />
+      <Projects />
     </>
   )
 }
