0f82bd731a9c9aec823da43c0995bf5302377f1e
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-08 10:19:57 +0100

    progress

---
+++ portfolio/pages/components/ProjectItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectItem = () => {
+  return (
+    <div>ProjectItem</div>
+  )
+}
+
+export default ProjectItem
--- portfolio/pages/components/Projects.jsx
+++ portfolio/pages/components/Projects.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import propertyImg from '../../public/assets/projects/property.jpg'
+import cryptoImg from '../../public/assets/projects/crypto.jpg'
+import netflixImg from '../../public/assets/projects/netflix.jpg'
+import twitchImg from '../../public/assets/projects/twitch.jpg'
+import ProjectItem from './ProjectItem.jsx'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div id='projects' className='w-full'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+            <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Projects:</p>
+            <h2 className='py-4'>What I&apos;ve Built</h2>
+            <div className='grid md:grid-cols-2 gap-8'>
+                <ProjectItem title='Property Finder' backgroundImg={propertyImg} projectUrl='/property' tech='React JS' />
+                <ProjectItem title='Crypto App' backgroundImg={cryptoImg} projectUrl='/crypto' tech='React JS' />
+                <ProjectItem title='Netflix App' backgroundImg={netflixImg} projectUrl='/netflix' tech='React JS' />
+                <ProjectItem title='Twitch App' backgroundImg={twitchImg} projectUrl='/twitch' tech='Next JS' />
+            </div>
+        </div>
+    </div>
   )
 }
 
