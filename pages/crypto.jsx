761b3a0ed4a9911e69af1a6c78140490c5e3dff1
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-09 15:45:39 +0100

    summary

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -11,8 +11,8 @@ const crypto = () => {
         <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
             <Image className='absolute z-1' layout='fill' objectFit='cover' src={cryptoImg} alt='/' />
             <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-            <h2 className='py-2'>Crypto App</h2>
-            <h3>React JS / Tailwind / Firebase</h3>
+                <h2 className='py-2'>Crypto App</h2>
+                <h3>React JS / Tailwind / Firebase</h3>
             </div>
         </div>
 
